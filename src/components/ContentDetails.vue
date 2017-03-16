edb7678769033173c7953732f40f3dbc1ee3d0a2
Keiichi Lindley <digizn@gmail.com>
2017-03-16 13:41:09 -0700

    Add image size label

--- src/components/ContentDetails.vue
+++ src/components/ContentDetails.vue
@@ -278,6 +278,7 @@
             <div class="ui bottom attached tab segment" data-tab="assets">
                 <div class="preview-image" v-for="image in asset.images">
                   <img class="ui image" :src="image.url" :alt="image.url" :title="image.url" >
+                  <div class="ui bottom left attached label">{{ image.size }}</div>
                   <div class="ui divider"></div>
                 </div>
             </div>
